c8ebd1e2ab970cefee685ccb39d3817678d91f1c
Daniel Bravo <daniel.brv@hotmail.cl>
2021-08-22 20:55:48 -0400

    Se agrega la clase 2 Clase 2 LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ newFunction2('Ricardo', 27, "CL")
 
 
 //Concatenación es5
-
 let hello = "Hello";
 let world = "World";
 let epicPhrase = hello + ' ' + world;
@@ -26,3 +25,55 @@ console.log(epicPhrase);
 //es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+
+//Clase 2 LET y CONST, Multilínea, Spread Operator y Desestructuración
+
+//mutilínea
+//es5
+let lorem = "Quiero escribir una frase epica. La cual debe ser separada \n"
++ "otra frase epica"
+//es6
+let lorem2 = `Super frase espicaaaa 2
+ahora es otra frase epica en ecmascript 6
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+//Desestructuración
+let person = {
+    'name': 'oscar',
+    'age': '32',
+    'country': 'MX'
+}
+//es5
+console.log(person.name,person.age,person.country);
+//es6
+let {name,age,country} = person;
+console.log(name,age,country);
+
+//operador de propagación
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+//Nuevas variables
+{
+    var globalVar = "Global Var";
+}
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+console.log(globalVar);
+
+const a = 'b';
+a = 'a';
+console.log(a);
+
+// clase 3 Arrow Functions, Promesas y Parámetros en objetos
